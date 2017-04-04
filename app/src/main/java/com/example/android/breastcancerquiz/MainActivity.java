f4423ab43553bdbf9f2f893d1f1bb75b53a00c7c
Moonminky <noremori@gmail.com>
2017-04-04 21:13:31 +0200

    Code & comment cleanup, added method to hide keyboard when EditText out of focus

--- app/src/main/java/com/example/android/breastcancerquiz/MainActivity.java
+++ app/src/main/java/com/example/android/breastcancerquiz/MainActivity.java
@@ -1,17 +1,29 @@
 package com.example.android.breastcancerquiz;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
+
 public class MainActivity extends AppCompatActivity {
 
     int score, q1, q2, q3, q4, q5a1, q5a2, q5a3, q6, q7;
+
+    //Hide Keyboard when not needed
+    public static void hideKeyboard(Activity activity) {
+        if (activity != null && activity.getWindow() != null && activity.getWindow().getDecorView() != null) {
+            InputMethodManager imm = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(activity.getWindow().getDecorView().getWindowToken(), 0);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,13 +31,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    /**
-     * This code should
-     * run through each question in the app,
-     * record whether the user got the question right
-     * and then display the quiz results in a Toast message.
-     */
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        View v = getCurrentFocus();
 
+        if (v != null &&
+                (ev.getAction() == MotionEvent.ACTION_UP || ev.getAction() == MotionEvent.ACTION_MOVE) &&
+                v instanceof EditText &&
+                !v.getClass().getName().startsWith("android.webkit.")) {
+            int scrcoords[] = new int[2];
+            v.getLocationOnScreen(scrcoords);
+            float x = ev.getRawX() + v.getLeft() - scrcoords[0];
+            float y = ev.getRawY() + v.getTop() - scrcoords[1];
+
+            if (x < v.getLeft() || x > v.getRight() || y < v.getTop() || y > v.getBottom())
+                hideKeyboard(this);
+        }
+        return super.dispatchTouchEvent(ev);
+    }
+
+    //Check and grade Radiobutton selection
     public void onRadioButtonClicked(View view) {
         // Is the button now checked?
         boolean checked = ((RadioButton) view).isChecked();
@@ -34,66 +59,55 @@ public class MainActivity extends AppCompatActivity {
         switch (view.getId()) {
             case R.id.Q1A1:
                 if (checked)
-                    // Pirates are the best
                     q1 = 1;
                 break;
             case R.id.Q1A2:
                 if (checked)
-                    // Ninjas rule
                     q1 = 0;
                 break;
             case R.id.Q2A1:
                 if (checked)
-                    // Ninjas rule
                     q2 = 1;
                 break;
             case R.id.Q2A2:
                 if (checked)
-                    // Ninjas rule
                     q2 = 0;
                 break;
             case R.id.Q3A1:
                 if (checked)
-                    // Ninjas rule
                     q3 = 0;
                 break;
             case R.id.Q3A2:
                 if (checked)
-                    // Ninjas rule
                     q3 = 0;
                 break;
             case R.id.Q3A3:
                 if (checked)
-                    // Ninjas rule
                     q3 = 1;
                 break;
             case R.id.Q4A1:
                 if (checked)
-                    // Ninjas rule
                     q4 = 0;
                 break;
             case R.id.Q4A2:
                 if (checked)
-                    // Ninjas rule
                     q4 = 1;
                 break;
             case R.id.Q4A3:
                 if (checked)
-                    // Ninjas rule
                     q4 = 0;
                 break;
             case R.id.Q6A1:
                 if (checked)
-                    // Ninjas rule
                     q6 = 0;
                 break;
             case R.id.Q6A2:
                 if (checked)
-                    // Ninjas rule
                     q6 = 1;
         }
     }
 
+    //Check and grade Checkbox selection
     public void onCheckboxClicked(View view) {
         // Is the view now checked?
         boolean checked = ((CheckBox) view).isChecked();
@@ -120,7 +134,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    //Build score
+    //Check EditText questions and Build score
     public void submitAnswers(View view) {
         EditText Q7 = (EditText) findViewById(R.id.Q7A1);
         String Q7A1 = Q7.getEditableText().toString();
@@ -129,7 +143,6 @@ public class MainActivity extends AppCompatActivity {
             q7 = 1;
         }
         score = q1 + q2 + q3 + q4 + q5a1 + q5a2 + q5a3 + q6 + q7;
-        Log.d("score", "score: " + score);
         showResults(score);
     }
 
@@ -144,8 +157,7 @@ public class MainActivity extends AppCompatActivity {
         }
         if (score > 5 && score < 7) {
             Toast.makeText(getApplicationContext(), getString(R.string.resultMore5, score), Toast.LENGTH_LONG).show();
-        }
-        else  if (score ==7) {
+        } else if (score == 7) {
             Toast.makeText(getApplicationContext(), getString(R.string.resultPerfect), Toast.LENGTH_LONG).show();
         }
     }
